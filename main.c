02b86858c2f2099982a628bf3a878b032f856d38
rodrigokf <rodrigo.kf@hotmail.com>
2016-06-03 22:19:39 -0300

    digao
    soh umas mudançinhas pra testar o github

--- main.c
+++ main.c
@@ -1,32 +1,37 @@
-﻿#include <stdio.h>
-#include <allegro5/allegro.h>
+﻿#include <stdlib.h>
+#include <stdio.h>
 
+#include "allegro5/allegro.h"
 #include "menu.h"
-
 
 int main(int argc, char **argv){
 
+    const int Largura_t = 640;
+    const int Altura_t = 480;
+
+    //Inicialização dos Addons(modulos)
 	al_init();
 	al_init_font_addon();
 	al_init_ttf_addon();
 	al_init_primitives_addon();
 
+    //Inicializa o Display
 	ALLEGRO_DISPLAY *display = NULL;
-	
-	display = al_create_display(640, 480);
+	display = al_create_display(Largura_t, Altura_t);
 	if (!display) {
 		fprintf(stderr, "failed to create display!\n");
 		return -1;
 	}
-
+    //Inicializa o Display com fundo Preto
 	al_clear_to_color(al_map_rgb(0, 0, 0));
-	
+    //Chama Funcao q desenha o menu
 	desenha_menu(display);
-
+    //Flip na Tela
 	al_flip_display();
 
 	al_rest(10.0);
 
+    //Desaloca Memoria
 	al_destroy_display(display);
 
 	return 0;
--- menu.c
+++ menu.c
@@ -5,12 +5,12 @@ int inicializa_menu(){
 	fonte_opcoes = al_load_ttf_font("C:/Labirinto/fontes/kristen.ttf", 36, 0);
 	if (!fonte_titulo || !fonte_opcoes)
 	{
-		//printf("\nFalha ao carregar arquivo da fonte de texto!!!");
+		printf("\nFalha ao carregar arquivo da fonte de texto!!!");
 		return 0;
 	}
 
 	cor_selecionado = al_map_rgb(0, 150, 150);
-	
+
 	opcao_selecionada = 0;
 
 	return 1;
@@ -18,8 +18,9 @@ int inicializa_menu(){
 
 void desenha_menu(ALLEGRO_DISPLAY *display){
 	inicializa_menu();
-	al_draw_text(fonte_titulo, al_map_rgb(255, 255, 255), 640/2, 480/6, ALLEGRO_ALIGN_CENTER, "LABIRINTO");
-	
+	al_draw_text(fonte_titulo, al_map_rgb(255, 255, 255), 640/2, 480/4, ALLEGRO_ALIGN_CENTER, "LABIRINTO");
+	al_draw_line(100, 170, 540, 170, al_map_rgb(255,255,255), 5);
+
 	if (opcao_selecionada == NOVO_JOGO) al_draw_text(fonte_opcoes, cor_selecionado, 640 / 2, 480 / 2, ALLEGRO_ALIGN_CENTER, "NOVO JOGO");
 	else al_draw_text(fonte_opcoes, al_map_rgb(255, 255, 255), 640 / 2, 480 / 2, ALLEGRO_ALIGN_CENTER, "NOVO JOGO");
 	al_draw_text(fonte_opcoes, al_map_rgb(255, 255, 255), 640 / 2, 480 / 2 + 40, ALLEGRO_ALIGN_CENTER, "INSTRUCOES");
